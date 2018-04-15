055a89665e06d294b17f58aba0bd66d9cf2d8c74
Teo <matteosistisette@gmail.com>
2018-04-15 19:35:37 +0200

    Removed some temporary debug code I had added

--- GxEPD2_32_3C.cpp
+++ GxEPD2_32_3C.cpp
@@ -41,8 +41,6 @@ GxEPD2_32_3C::GxEPD2_32_3C(GxEPD2::Panel panel, int8_t cs, int8_t dc, int8_t rst
 
 void GxEPD2_32_3C::drawPixel(int16_t x, int16_t y, uint16_t color)
 {
-  int orgx=x;
-  int orgy=y;
   if ((x < 0) || (x >= width()) || (y < 0) || (y >= height())) return;
   if (_mirror) x = width() - x - 1;
   // check rotation, move pixel around if necessary
@@ -64,24 +62,15 @@ void GxEPD2_32_3C::drawPixel(int16_t x, int16_t y, uint16_t color)
   uint16_t i = x / 8 + y * _width_bytes;
   if (_current_page < 1)
   {
-    
     if (i >= sizeof(_black_buffer)) return;
   }
   else
   {
-    y -= (_current_page * _page_height);
-    
+    y -= _current_page * _page_height;
     if ((y < 0) || (y >= _page_height)) return;
     i = x / 8 + y * _width_bytes;
-    
-    
   }
   
-  if ((x==0 || x==639) && (y<4 || y>184)) {
-	Serial.printf("drawPixel %03d %03d => %03d %03d", orgx, orgy, x, y);
-	Serial.println();
-  }
-	
   _black_buffer[i] = (_black_buffer[i] & (0xFF ^ (1 << (7 - x % 8)))); // white
   _red_buffer[i] = (_red_buffer[i] & (0xFF ^ (1 << (7 - x % 8)))); // white
   if (color == GxEPD_WHITE) return;
